1e549399553b6883ff556ae84e5aa5e5af9d4311
NeKit <nekit1000@gmail.com>
2020-05-08 00:42:13 +0200

    Override libmtk-ril.so in /vendor to fix incoming calls indication

--- usr/bin/droid/bind_vendor.sh
+++ usr/bin/droid/bind_vendor.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 mount -o ro,bind /usr/libexec/droid-hybris/vendor/lib64/hw/audio.primary.mt6771.so /vendor/lib64/hw/audio.primary.mt6771.so
+mount -o ro,bind /usr/libexec/droid-hybris/vendor/lib64/libmtk-ril.so /vendor/lib64/libmtk-ril.so
